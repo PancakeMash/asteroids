0fe7d3f790c8de5ead6b9b1d3a6c322f5102b694
PancakeMash <arpandevofficial@gmail.com>
2025-01-27 12:26:00 +0000

    B: Added asteroids and motion to the game.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -7,15 +11,41 @@ def main():
 	print("Screen width:", SCREEN_WIDTH)
 	print("Screen height:", SCREEN_HEIGHT)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
 
+	updatable = pygame.sprite.Group()
+	drawable =  pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	
+	player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT / 2))
+	asteroid_field = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return		
+			if event.type == pygame.QUIT:
+				return		
+
 		screen.fill("black")
+
+		for thing in drawable:
+			thing.draw(screen)
+		
+		for thing in updatable:
+			thing.update(dt)
+		
+		for asteroid in asteroids:
+			if player.collision(asteroid):
+				sys.exit("Game Over!")
+
+
 		pygame.display.flip()
 
-
+		dt = clock.tick(60)/1000
+		
 if __name__ == "__main__":
     main()
